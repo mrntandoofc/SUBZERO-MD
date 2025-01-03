fef3776ff3caa7627ccdc191800a33d22bf2a809
mrntandoofc <Lightmker7@gmail.com>
2025-01-04 00:58:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-//--------------------------Mr Frank---------------------------------------
+//--------------------------Mr Ntando---------------------------------------
 module.exports = {
 
 SESSION_ID: process.env.SESSION_ID || "Subzero~WAYDxJ4b#xnbX8zYcry9SFGQhBVQ6BYcX7PIvmw3yXsKghOn5MNs",
@@ -34,7 +34,7 @@ AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/m2jtqrBC/Screenshot-20241216-090753-Video-Maker.jpg", // DO NOT CHANGE
-ALIVE_MSG: process.env.ALIVE_MSG || "sᴜʙᴢᴇʀᴏ ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *Zᴇʀᴏ Cᴏᴅᴇʀs❄️*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ ᴍʀ ꜰʀᴀɴᴋ",
+ALIVE_MSG: process.env.ALIVE_MSG || "sᴜʙᴢᴇʀᴏ ᴀɪ ɪs ᴀʟɪᴠᴇ. ᴋᴇᴇᴘ sᴜᴘᴘᴏʀᴛɪɴɢ *Ntando Cᴏᴅᴇʀs❄️*\n\n> ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ Mr Ntando",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || ".", // .,!/?
@@ -46,4 +46,4 @@ BOT_NAME: process.env.BOT_NAME || "『 ＳＵＢＺＥＲＯ - ＭＤ 』",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 
-//--------------MADE BY MR FRANK 2024 DEC---------------->-
+//--------------MADE BY MR Ntando 2024 DEC---------------->-
